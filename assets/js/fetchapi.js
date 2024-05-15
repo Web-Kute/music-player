533c0ce52359a5880dc2bc34e763708569ba197c
Ikigai <alvardsandz@gmail.com>
2024-05-15 15:24:14 +0200

    config index.js with module

---
+++ assets/js/fetchapi.js
@@ -0,0 +1,11 @@
+const baseURL = 'https://api.deezer.com/';
+console.log(baseURL);
+export async function fetchMusicData(endpoint) {
+  const response = await fetch(baseURL + endpoint);
+
+  const data = await response.json();
+  return data;
+}
+
+// https://developers.deezer.com/guidelines/getting_started
+//  https://connect.deezer.com/oauth/auth.php?app_id=yesouican&redirect_uri=http://localhost:8888/music-player/&perms=basic_access,email
